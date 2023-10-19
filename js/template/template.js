f238910f73d5dc4d10d46ad45efe5b5ec0f7793b
Mystlicious <Daffaaudyapramana03@gmail.com>
2023-10-19 17:38:31 +0700

    coba

--- js/template/template.js
+++ js/template/template.js
@@ -1,15 +1,16 @@
-export let URLGeoJson = "https://asia-southeast2-rock-prism-401900.cloudfunctions.net/chapter3gis";
-export let tableTag="tr";
-export let tableRowClass="content is-small";
-export let tableTemplate=`
+export let URLGeoJson =
+  "https://asia-southeast2-adept-antler-402414.cloudfunctions.net/function";
+export let tableTag = "tr";
+export let tableRowClass = "content is-small";
+export let tableTemplate = `
 <td>#NAME#</td>
 <td >#KORDINAT#</td>
 <td>#TYPE#</td>
-`
+`;
 export const clickpopup = `
 Long : #LONG#<br>
 Lat  : #LAT#<br>
 X   : #X#<br>
 Y   : #Y#<br>
 HDMS : #HDMS#<br>
-`
+`;
